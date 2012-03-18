f91f35b5db74ea03d34048f314b57289e1046a32
Gautam <gautham5678@gmail.com>
2012-03-18 19:21:52 +0530

    Changes to Forms and App controller, Added Security Component

--- dynForms/Controller/FormsController.php
+++ dynForms/Controller/FormsController.php
@@ -11,6 +11,29 @@
  * @author gautam
  */
 class FormsController extends AppController {
+    
+    
+   
+    
+    public function _crsf_error(){
+        $this->flash("CRSF Error", array("controller"=>"Pages","action"=>"display"));
+    }
+
+    public function blackhole($type) {
+        switch ($type) {
+            case 'csrf':
+                $this->_crsf_error();
+                break;
+
+            default:
+                break;
+        }
+    }
+    
+    public function beforeFilter() {
+        $this->Security->blackHoleCallback = 'blackhole';
+    }
+    
     public $helpers=array("Form","Html");
     public function getForm($id=null){
         if(is_null($id) == true ){
@@ -28,5 +51,10 @@ class FormsController extends AppController {
         
         $this->set("form", $results);
     }
+    
+    public function form_response() {
+        
+    }
+    
 }
 
