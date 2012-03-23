a6b928975a42011a9a35b6dde74260bb7d0f17db
Gautam <gautham5678@gmail.com>
2012-03-23 07:50:13 +0530

    Need to get default values working in DynamicFormsResponse check stackoverflow http://stackoverflow.com/q/9833315/492561

--- dynForms/Model/DynamicFormResponse.php
+++ dynForms/Model/DynamicFormResponse.php
@@ -7,6 +7,20 @@ class DynamicFormResponse extends AppModel{
     public $primaryKey = '_id';
     public $validate=array();
     
+    public $mongoschema = array(
+        'created' => array('type' => 'datetime'),
+        'modified' => array('type' => 'datetime'),
+        'escalation'=>array(
+            'type'=>"integrer",
+            "default value"=>0
+            ),
+        "status"=>array(
+            "type"=>"string",
+            "default value"=>"pending"
+            ),
+        
+    );
+    
     public function setSchema($schema) {
         $this->_schema=$schema;        
     }
